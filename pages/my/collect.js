d64d52e58fd02325f48f7d2f020dee172465121f
Tockey <tockey@yeah.net>
2024-12-02 19:33:32 +0800

    完成：我的收藏界面布局

---
+++ pages/my/collect.js
@@ -0,0 +1,137 @@
+// pages/my/collect.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+    navHeight: wx.getSystemInfoSync().statusBarHeight + 44,
+    // 收藏列表
+    collectList: [
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线上直播',
+        title: '西门子 TIA 探厂记 —— 走进乐惠国际，共启一罐啤酒的“新鲜”之旅',
+        date: '2023-01-01',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线下活动',
+        title: '2024 CSEAC 半导体行业展会',
+        date: '2023-02-01',
+        selected: false
+      }
+    ],
+    // 全选
+    selectAll: false,
+    // 弹窗
+    showPopTip: false
+  },
+
+  selectActivity(e) {
+    const index = e.currentTarget.dataset.index;
+    this.data.collectList[index].selected = !this.data.collectList[index].selected;
+    this.setData({
+      collectList: this.data.collectList
+    });
+  },
+  selectAll() {
+    this.data.selectAll = !this.data.selectAll;
+    this.data.collectList.forEach(item => {
+      item.selected = this.data.selectAll;
+    });
+    this.setData({
+      collectList: this.data.collectList,
+      selectAll: this.data.selectAll
+    });
+  },
+
+  // 弹窗按钮事件
+  popTipHandler(e) {
+    // console.log(e);
+    if (e.type === 'cancel') {
+      this.setData({
+        showPopTip: false
+      });
+    } else {
+      console.log('确认');
+    }
+  },
+
+  // 删除按钮事件
+  deleteBtnHandler() {
+    console.log('删除');
+
+    // 判断是否选择了数据
+    const selectedList = this.data.collectList.filter(item => item.selected);
+    if (selectedList.length === 0) {
+      wx.showToast({
+        title: '请选择要删除的数据',
+        icon: 'none'
+      });
+      return;
+    }
+
+    this.setData({
+      showPopTip: true
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- pages/my/my.js
+++ pages/my/my.js
@@ -13,7 +13,7 @@ Page({
       { icon: '../../static/my/2382.svg', title: '我的签到', url: '/pages/my/attendance' },
       { icon: '../../static/my/2386.svg', title: '我的问卷', url: '/pages/my/questionnaire' },
       { icon: '../../static/my/2369.svg', title: '我的消息', url: '' },
-      { icon: '../../static/my/2377.svg', title: '我的收藏', url: '' },
+      { icon: '../../static/my/2377.svg', title: '我的收藏', url: '/pages/my/collect' },
       { icon: '../../static/my/2375.svg', title: '邀请好友', url: '/pages/my/invite_friend' },
       { icon: '../../static/my/2361.svg', title: '在线咨询', url: '' },
       { icon: '../../static/my/2371.svg', title: '订阅消息', url: '' }
