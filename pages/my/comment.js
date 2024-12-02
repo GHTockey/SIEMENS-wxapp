9cecd4d895b3b0b0d546ff00e95a83fd38fca54d
Tockey <tockey@yeah.net>
2024-12-02 17:50:46 +0800

    完成：我的界面布局

--- pages/my/comment.js
+++ pages/my/comment.js
@@ -5,7 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
+    statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+    navHeight: wx.getSystemInfoSync().statusBarHeight + 44,
+    // 评论列表
+    commentList: [
+      {
+        date: '2024-09-10 12:00',
+        title: '活动举办成功',
+        desc: '西门子 TIA 探厂记 —— 走进乐惠国际， 共启一罐啤酒的“新鲜”之旅',
+        selected: true
+      },
+      {
+        date: '2024-09-11 14:00',
+        title: '活动圆满结束',
+        desc: '2024 CSEAC 半导体行业展会',
+        selected: false
+      },
+      {
+        date: '2024-09-11 14:00',
+        title: '很棒的体验',
+        desc: 'TIA 博途 · 点控未来 - 济南站',
+        selected: false
+      },
+    ],
+  },
 
+  // 切换评论状态
+  switchCommentStatus(e) {
+    const index = e.currentTarget.dataset.index;
+    this.data.commentList[index].selected = !this.data.commentList[index].selected;
+    this.setData({
+      commentList: this.data.commentList
+    });
   },
 
   /**
