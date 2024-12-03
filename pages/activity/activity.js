1ba585ac52addebdd9ca76cefbfb105444ab5427
Tockey <tockey@yeah.net>
2024-12-03 14:39:04 +0800

    完成：活动中心瀑布流布局

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -5,7 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
+    statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+    navHeight: wx.getSystemInfoSync().statusBarHeight + 44,
+    // 是否订阅
+    isSubscribe: false,
+    // 日历
+    isCalendar: true,
+    // 活动列表
+    activityList: [
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线上直播',
+        title: '西门子 TIA 探厂记 —— 走进乐惠国际，共启一罐啤酒的“新鲜”之旅旅旅旅旅旅旅旅',
+        date: '2023-01-01 10:00',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1434.png',
+        tag: '线上直播',
+        title: '2024 CSEAC 半导体行业展会',
+        date: '2023-01-01 10:00',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1434.png',
+        tag: '线上直播',
+        title: '西门子智能制造交流研讨会 （齐齐哈尔站）',
+        date: '2023-01-01 10:00',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线上直播',
+        title: 'TIA 博途 · 点控未来 — 济南站',
+        date: '2023-01-01 10:00',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线上直播',
+        title: '西门子 TIA 探厂记',
+        date: '2023-01-01 10:00',
+        selected: false
+      },
+      {
+        imgSrc: '../../static/1346.png',
+        tag: '线下活动',
+        title: '2024 CSEAC 半导体行业展会',
+        date: '2023-02-01 10:00',
+        selected: false
+      }
+    ],
+  },
 
+  // 切换订阅/日历
+  switchSubscribeOrCalendar(e) {
+    let type = e.currentTarget.dataset.type
+    this.setData({
+      [`is${type}`]: !this.data[`is${type}`]
+    })
   },
 
   /**
